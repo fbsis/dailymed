63cf769b83fd67f03003c303d88b64164a573e61
Felipe Braga <git@fbsis.com>
2025-05-28 11:46:36 -0300

    test: adds initial test for indication entity
    Creates a basic test to check existence of the indication entity, laying groundwork for future test coverage and validation.

---
+++ src/tests/domain/entities/Indication.test.ts
@@ -0,0 +1,13 @@
+import { Indication } from '@/domain/entities/Indication';
+
+describe('Indication', () => {
+  describe('instantiation', () => {
+    it('should exist', () => {
+      expect(Indication).toBeDefined();
+    });
+
+  });
+
+
+  
+}); 
